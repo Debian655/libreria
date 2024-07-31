2ab409a5380c601b56afcba058d0873c0740792c
Debian655 <debiantrabajo@hotmail.com>
2024-07-31 12:52:32 -0600

    actualizacion

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template
+from auth import auth
 
 app = Flask(__name__)
-
+app.register_blueprint(auth, url_prefix='/auth')
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
-
-@app.route('/signup')
-def signup():
-    return render_template("signup.html")
-
-@app.route('/send-email')
-def recordar():
-    return render_template("send-email.html")
